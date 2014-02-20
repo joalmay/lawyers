e9ed044745163fec4629db9eb30615b520c803cb
joalmay <joalmay@gmail.com>
2014-02-19 22:22:17 -0700

    Step3

--- server.js
+++ server.js
@@ -3,27 +3,81 @@ var Sequelize = require('sequelize')
   // Look to the next section for possible options
 });
 
- /*
+//App Port
+var port = 8888;
 
-  , sequelize = new Sequelize('lawyer_scoreboard', 'root', 'qaz', {
-  host: "192.168.1.175",
-  port: 3306
-});
- 
- */
+//Define Lawyer
 var Lawyer = sequelize.define('lawyer', {
+  id: Sequelize.INTEGER,
   name: Sequelize.STRING,
-  wins: Sequelize.INTEGER
+  location: Sequelize.STRING,
+  wins: Sequelize.INTEGER,
+  losses: Sequelize.INTEGER,
+  hourly_rate: Sequelize.DECIMAL
 });
 
+//Array
 
- 
+var lawyersarray = [];
+
+var express = require('express');
+
+app = express();
+
+app.configure(function (){
+  app.use(express.bodyParser());
+  app.use(function(req, res, next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+});
+
+app.get('/lawyers', function(req, res) {
+  Lawyer.all().success(function(lawyers) {
+  // lawyers will be an array of all Lawyer instances
+  lawyersarray = lawyers;
+
+  res.type('application/json');
+  res.json({location: lawyers});
+
+});
+
+  sequelize.all
+});
+
+//Populate 
+
+ /*
 sequelize.sync().success(function() {
   Lawyer.create({
-    name: 'John',
-    wins: 3
+    name: 'Dallin',
+    location: 'Provo, CA',
+    wins: 7,
+    losses: 20,
+    hourly_rate: 10
+
   }).success(function(sdepold) {
     console.log(sdepold.values)
   });
 });
 
+ 
+sequelize.sync().success(function() {
+  Lawyer.create({
+    name: 'Dan',
+    location: 'Orem, UT',
+    wins: 34,
+    losses: 12,
+    hourly_rate: 10
+
+  }).success(function(sdepold) {
+    console.log(sdepold.values)
+  });
+});
+
+*/
+
+app.listen(port);
+console.log('Listening on port '+port);
